cf71e8d09d715acb5c6ccba68b1654b83e5edf9c
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2016-12-01 11:01:55 +0100

    updates

--- shiny2/server.R
+++ shiny2/server.R
@@ -3,16 +3,23 @@ library("openxlsx")
 library("rbokeh")
 library("htmlwidgets")
 library("ggplot2")
+library("grid")
+library("gridExtra")
 library("ggthemes")
 
-theGGPlot <- function(di, whiskers=TRUE, onX = "Number", unit = "ng/mL", logx=FALSE, smooth=FALSE) {
+
+theGGPlot <- function(di, whiskers=TRUE, onX = "Number", unit = "ng/mL", logx=FALSE, smooth=FALSE, bland=FALSE, showex=TRUE) {
   if (is.null(di))
     return(NULL)
-  pl <- ggplot(di, aes_string(x=onX, y="diff", color="absDiff")) 
-  if (whiskers) 
-    pl <- pl + 
+  if (onX != "Number" & bland) {
+      di$Initial_Value = di$Average
+  }
+
+  pl <- ggplot(di, aes_string(x=onX, y="diff", color="absDiff"))
+  if (whiskers)
+    pl <- pl +
       geom_linerange(data=di[abs(di$diff) > 20,], aes_string(x=onX, ymin="diff", ymax="diffSign", color="absDiff"))
-  pl <- pl + 
+  pl <- pl +
     geom_point() + theme_bw() +
     geom_hline(yintercept=0, linetype=2) +
     geom_hline(yintercept=c(20,-20)) +
@@ -21,17 +28,27 @@ theGGPlot <- function(di, whiskers=TRUE, onX = "Number", unit = "ng/mL", logx=FA
                        limits=c(min(-100,min(di$diff, na.rm=TRUE)),max(100, max(di$diff, na.rm=TRUE))))+
     scale_color_manual(values=c("black","red3"))+
     theme(legend.position="none", text=element_text(size=15))
-  if (smooth) 
+  if (smooth)
     pl <- pl + geom_smooth(se=FALSE, color="blue1", group=1, size=2, span=0.5, method="loess")
+  if (showex) {
+    di2 <- di[c(which.min(di$diff), which.max(di$diff)),]
+    pl <- pl + geom_text(data=di2, aes(label=Number), color="red2", group=1, vjust=c(1.5,-0.5))
+  }
 
   if (onX == "Number") {
-    pl + scale_x_continuous(expand = c(0,0), limits = c(0,1.2*max(di$Number))) + 
+    pl + scale_x_continuous(expand = c(0,0), limits = c(0,1.2*max(di$Number))) +
       xlab("Sample Number") + ggtitle("Difference vs. Sample Number")
   } else {
-    if (logx) 
+    # input$bland
+    if (logx)
       pl <- pl + scale_x_log10()
-    pl + xlab(paste("Initial Value",unit)) + ggtitle("Difference vs. Initial Value")
+    if (bland) {
+      pl <- pl + xlab(paste("Mean Value",unit)) + ggtitle("Difference vs. Mean Value")
+    } else {
+      pl <- pl + xlab(paste("Initial Value",unit)) + ggtitle("Difference vs. Initial Value")
+    }
   }
+  pl
 }
 
 function(input, output) {
@@ -42,32 +59,31 @@ function(input, output) {
       return(NULL)
     df <- read.xlsx(inFile$datapath, "Arkusz1", startRow=3)
     colnames(df) <- c(
-      "Number",	"Subject", "Period", "Sampling_Point", "Initial_Value",	"Repeat_Value",	"diff",	"ISR" 
+      "Number",	"Subject", "Period", "Sampling_Point", "Initial_Value",	"Repeat_Value",	"diff",	"ISR"
     )
     df <- na.omit(df[,c(1,5:8)])
     df$absDiff <- abs(df$diff) > 20
     df$diffSign <- 20*sign(df$diff)
+    df$Average <- (df$Initial_Value + df$Repeat_Value)/2
     attr(df, which = "name") = inFile[[1]]
     df
   })
-  
+
   output$ggPlotDiffNo <- renderPlot({
     di <- dataInput()
-    theGGPlot(di, whiskers = input$whiskers, smooth = input$smooth) 
+    theGGPlot(di, whiskers = input$whiskers, smooth = input$smooth, showex = input$showex)
   })
-  
+
   output$ggPlotDiffInit <- renderPlot({
     di <- dataInput()
-    theGGPlot(di, whiskers = input$whiskers, onX="Initial_Value", unit = input$unit, logx = input$logx, smooth = input$smooth) 
+    theGGPlot(di, whiskers = input$whiskers, onX="Initial_Value",
+              unit = input$unit, logx = input$logx, smooth = input$smooth, bland=input$bland, showex = input$showex)
   })
-  
-  output$ggPlotDiffECDF <- renderPlot({
-    di <- dataInput()
-    if (is.null(di)) return(NULL)
-    
+
+  prepareGgPlotDiffECDF <- function(di) {
     frac <- mean(abs(di$diff)<=20)
     pl <- ggplot(di, aes(abs(diff))) +
-      stat_ecdf() + 
+      stat_ecdf() +
       theme_bw() +
       geom_linerange(x=20, ymin=0, ymax=frac, linetype=2) +
       geom_hline(yintercept=frac, linetype=2, color="red3") +
@@ -77,77 +93,115 @@ function(input, output) {
       theme(legend.position="none", text=element_text(size=15)) +
       ggtitle("Distribution of absolute differences")
     pl
+  }
+
+  output$ggPlotDiffECDF <- renderPlot({
+    di <- dataInput()
+    if (is.null(di)) return(NULL)
+    prepareGgPlotDiffECDF(di)
   })
-  
-  
+
+  output$downloadData <- downloadHandler(
+   filename = function() {
+     paste('ISR_report_', Sys.Date(), '.pdf', sep='')
+   },
+   content = function(con) {
+     di <- dataInput()
+     pdf(con, width = 12, height = 18)
+     if (is.null(di)) {
+       grid::grid.newpage()
+       grid::grid.text('Please, first upload a file with IRS data')
+     } else {
+       pl1 <- prepareGgPlotISR(di, input$ciband)
+       pl2 <- theGGPlot(di, whiskers = input$whiskers, smooth = input$smooth, showex = input$showex)
+       pl3 <- theGGPlot(di, whiskers = input$whiskers, onX="Initial_Value", unit = input$unit, logx = input$logx, smooth = input$smooth, bland=input$bland, showex = input$showex)
+       pl4 <- prepareGgPlotHist(di)
+       pl5 <- prepareGgPlotDiffECDF(di)
+       desc <- paste0(prepareDesc(di), "\n\nReport generated with ISRgenerator\nhttp://_more_info_here_")
+       pl6 <- textGrob(desc,gp=gpar(fontsize=16)) #, hjust=0, just=0
+       print(grid.arrange(pl2, pl3, pl4, pl5, pl1, pl6, ncol=2, top =textGrob( "Incurred Sample Reanalysis Report\n ",gp=gpar(fontsize=40,font=3))))
+     }
+     dev.off()
+   }
+ )
+
+
+  prepareGgPlotISR <- function(di, input.ciband) {
+    frac <- mean(abs(di$diff)<=20)
+    di$succ   <- cumsum(abs(di$diff)<=20)
+    di$trials <- 1:nrow(di)
+    di$cumm   <- di$succ / di$trials
+    ci <- ifelse(input.ciband == "50%", 0.5, 0.9)
+    CI <- t(sapply(1:nrow(di), function(i){
+      prop.test(di$succ[i], di$trials[i], conf.level = ci, correct = TRUE)$conf.int
+    }))
+    di$CI1 <- CI[,1]
+    di$CI2 <- CI[,2]
+    yname <- "ISR [%]"
+    pl <- ggplot(di, aes(Number, cumm))
+    if (input$ciband != "none") {
+      pl <- pl + geom_ribbon(aes(x=Number, ymin=CI1, ymax=CI2), fill="grey", alpha=0.5)
+      yname <- paste(yname, "± CI:", input$ciband)
+    }
+    pl <- pl +
+      geom_point() +
+      geom_line() +
+      theme_bw() +
+      geom_text(x=max(di$Number), y=frac, label=paste(round(100*frac,1), "%"), vjust=1, hjust=-0.1, color="red", size=6) +
+      geom_hline(yintercept=0.666, linetype=2) +
+      scale_y_continuous(limits=c(0,1), expand = c(0,0), breaks = round(c(seq(0,1,.2),0.666),2), name=yname,labels = scales::percent) +
+      scale_x_continuous(expand = c(0,0), limits = c(0,1.2*max(di$Number)), name="Sample Number") +
+      theme(legend.position="none", text=element_text(size=15))+
+      ggtitle("Cummulative ISR plot")
+    pl
+  }
+
   output$ggPlotISR <- renderPlot({
     di <- dataInput()
     if (is.null(di)) {
       return(grid::grid.text('Please, first upload a file with IRS data'))
     }
 
-    frac <- mean(abs(di$diff)<=20)
-    
-    di$succ   <- cumsum(abs(di$diff)<=20)
-    di$trials <- 1:nrow(di)
-    di$cumm   <- di$succ / di$trials
-
-    ci <- ifelse(input$ciband == "50%", 0.5, 0.9)
-    
-    CI <- t(sapply(1:nrow(di), function(i){
-      prop.test(di$succ[i], di$trials[i], conf.level = ci, correct = TRUE)$conf.int
-    }))
-    di$CI1 <- CI[,1]
-    di$CI2 <- CI[,2]
-    
-    yname <- "ISR [%]"
-    
-    pl <- ggplot(di, aes(Number, cumm)) 
-    if (input$ciband != "none") {
-      pl <- pl + geom_ribbon(aes(x=Number, ymin=CI1, ymax=CI2), fill="grey", alpha=0.5)
-      yname <- paste(yname, "± CI:", input$ciband)
-    } 
-    pl <- pl +
-      geom_point() +
-      geom_line() +
-      theme_bw() +
-      geom_text(x=max(di$Number), y=frac, label=paste(round(100*frac,1), "%"), vjust=1, hjust=-0.1, color="red", size=6) +
-      geom_hline(yintercept=0.666, linetype=2) + 
-      scale_y_continuous(limits=c(0,1), expand = c(0,0), breaks = round(c(seq(0,1,.2),0.666),2), name=yname,labels = scales::percent) +
-      scale_x_continuous(expand = c(0,0), limits = c(0,1.2*max(di$Number)), name="Sample Number") +
-      theme(legend.position="none", text=element_text(size=15))+
-      ggtitle("Cummulative ISR plot")
-    pl
+    prepareGgPlotISR(di, input$ciband)
   })
-  
-  output$ggPlotHist <- renderPlot({
-    di <- dataInput()
-    if (is.null(di)) return(NULL)
-    
+
+  prepareGgPlotHist <- function(di) {
     pl <- ggplot(di, aes(diff)) +
       geom_histogram(breaks=seq(-100,100,10), col="white") +
-      geom_vline(xintercept=20, linetype=2, col="red3") + 
-      geom_vline(xintercept=-20, linetype=2, col="red3") + 
-      theme_bw() + 
+      geom_vline(xintercept=20, linetype=2, col="red3") +
+      geom_vline(xintercept=-20, linetype=2, col="red3") +
+      theme_bw() +
       scale_fill_manual(values=c("black","red3"))+
       scale_y_continuous(name="Number of Samples",expand = c(0,0)) +
       scale_x_continuous(name="Difference [%]", expand = c(0,0), breaks = seq(-100, 100, 20)) +
       theme(legend.position="none", text=element_text(size=15)) +
       ggtitle("Histogram of differences")
     pl
+  }
+
+  output$ggPlotHist <- renderPlot({
+    di <- dataInput()
+    if (is.null(di)) return(NULL)
+    prepareGgPlotHist(di)
   })
-  
+
+  prepareDesc <- function(di) {
+    fn <- attr(di, which = "name")
+    paste0(
+      "File name: ", fn, "\n\n",
+      "Number of samples: ", nrow(di), "\n",
+      "The most different sample: ", which.max(abs(di$diff)), "\n",
+      "The largest absolute difference: ", signif(max(abs(di$diff)), 2), "% \n",
+      "Fraction of samples with difference < 20%: ", round(100*mean(abs(di$diff) < 20),2), "% \n",
+      "\nDate of the analysis: ", as.character(Sys.Date()), " \n"
+    )
+  }
+
   output$textSummary <- renderPrint({
     di <- dataInput()
     if (is.null(di)) return(cat(""))
+    cat(prepareDesc(di))
     fn <- attr(di, which = "name")
-    cat("File name: ", fn, "\n\n")
-    cat("Number of samples: ", nrow(di), "\n")
-    cat("The most different sample: ", which.max(abs(di$diff)), "\n")
-    cat("The largest absolute difference: ", signif(max(abs(di$diff)), 2), "% \n")
-    cat("Fraction of samples with difference < 20%: ", round(100*mean(abs(di$diff) < 20),2), "% \n")
-    cat("\nDate of the analysis: ", as.character(Sys.Date()), " \n")
-    cat("\nDownload a one page report [pdf]\n")
   })
-  
+
 }
--- shiny2/ui.R
+++ shiny2/ui.R
@@ -5,19 +5,24 @@ library("htmlwidgets")
 fluidPage(
   fluidRow(class = "myRow1",
     column(width = 4, h2("Incurred Sample Reanalysis"), p("Link to the article, contact email")),
-    column(width = 4, 
+    column(width = 4,
            fileInput('file1', 'Upload an XLSX or CSV file',
-                     accept=c('.csv', 'application/xlsx', 
-                              'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 
-                              '.xlsx'))),
-    column(width = 2, 
+                     accept=c('.csv', 'application/xlsx',
+                              'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                              '.xlsx')),
+           br(),
+             downloadLink('downloadData', 'Download the onepager')
+           ),
+    column(width = 2,
            p("Other options:"),
            checkboxInput("whiskers", label = "Whiskers", value = TRUE),
            checkboxInput("logx", label = "Log values", value = FALSE),
-           checkboxInput("smooth", label = "Smoothed trend", value = FALSE)),
-    column(width = 2, 
+           checkboxInput("smooth", label = "Smoothed trend", value = FALSE),
+           checkboxInput("bland", label = "Average instead of Initial", value = FALSE)),
+    column(width = 2,
            textInput("unit", label = "Units", value = "[ng/mL]"),
-           selectInput("ciband", label = "CI interval", choices = c("none", "50%", "90%"), selected = "50%"))
+           selectInput("ciband", label = "CI interval", choices = c("none", "50%", "90%"), selected = "50%"),
+           checkboxInput("showex", label = "Show extremes", value = TRUE))
   ),
   fluidRow(
     column(width = 4, plotOutput('ggPlotISR')),
@@ -31,7 +36,7 @@ fluidPage(
     column(width = 4,  plotOutput('ggPlotHist')),
     column(width = 4,  plotOutput('ggPlotDiffECDF')),
     column(width = 4,  br(),  verbatimTextOutput('textSummary'))
-  ), 
+  ),
   tags$head(tags$style("
       .myRow1{background-color: #dddddd;}
       .myRow3{height:3px; background-color: #dddddd;}
